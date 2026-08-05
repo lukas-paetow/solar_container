5b296a18a3d2128321341743894bc571073c6e4d
lukas-paetow <lukas.paetow1@gmail.com>
2026-08-05 22:22:27 +0200

    added comments

--- controller.py
+++ controller.py
@@ -18,30 +18,31 @@ logging.basicConfig(
     force=True,
 )
 
-LEASE_NAME = "controller"
-LEASE_DURATION_SECONDS = 15
-
-POD_NAME = os.environ["POD_NAME"]
-POD_NAMESPACE = os.getenv("POD_NAMESPACE", "default")
-
-config.load_incluster_config()
-coordination_api = client.CoordinationV1Api()
 
 
 
-
+# checking if this current pod has the lease to control, or creating the lease
 def try_acquire_or_renew_lease() -> bool:
+    LEASE_NAME = "controller"
+    LEASE_DURATION_SECONDS = 15
+    POD_NAME = os.environ["POD_NAME"]
+    POD_NAMESPACE = os.getenv("POD_NAMESPACE", "default")
+    config.load_incluster_config()
+    coordination_api = client.CoordinationV1Api()
+
     now = datetime.now(timezone.utc)
 
     try:
+        # find existing lease
         lease = coordination_api.read_namespaced_lease(
             name=LEASE_NAME,
             namespace=POD_NAMESPACE,
         )
     except ApiException as exc:
+        # 404: lease doesn't exist yet. other error would be unexpected
         if exc.status != 404:
             raise
-
+        # create lease
         lease = client.V1Lease(
             metadata=client.V1ObjectMeta(name=LEASE_NAME),
             spec=client.V1LeaseSpec(
@@ -54,16 +55,19 @@ def try_acquire_or_renew_lease() -> bool:
         )
 
         try:
+            # lease now exists in this pod and we are good
             coordination_api.create_namespaced_lease(
                 namespace=POD_NAMESPACE,
                 body=lease,
             )
             return True
         except ApiException as create_exc:
+            # 409 is Conflict. this is here if both pods do this at the same time
             if create_exc.status == 409:
                 return False
             raise
 
+    # get existing lease info. we will update this info
     spec = lease.spec
     holder = spec.holder_identity
     renew_time = spec.renew_time
@@ -76,18 +80,19 @@ def try_acquire_or_renew_lease() -> bool:
 
     if holder == POD_NAME:
         spec.renew_time = now
-
     elif expired:
+        # holder was other pod, now this pod takes over
         spec.holder_identity = POD_NAME
         spec.acquire_time = now
         spec.renew_time = now
         spec.lease_duration_seconds = LEASE_DURATION_SECONDS
         spec.lease_transitions = (spec.lease_transitions or 0) + 1
-
     else:
+        # other pod has valid lease
         return False
 
     try:
+        # actually update lease now
         coordination_api.replace_namespaced_lease(
             name=LEASE_NAME,
             namespace=POD_NAMESPACE,
